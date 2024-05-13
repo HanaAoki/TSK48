8b28256d42a77bcf531062be7500e21d9606cf18
DESKTOP-1E0P1LB\user43 <hana.aoki@comsize.com>
2024-05-13 11:48:15 +0900

    新規プロジェクト発足

---
+++ src/main/java/model/entity/CategoryBean.java
@@ -0,0 +1,29 @@
+package model.entity;
+
+public class CategoryBean {
+	private int categoryCode;
+	private String categoryName;
+	
+	public CategoryBean() {
+		
+	}
+
+	public int getCategoryCode() {
+		return categoryCode;
+	}
+
+	public void setCategoryCode(int categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+
+}
---
+++ src/main/java/servlet/ItemAddServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ItemAddServlet
+ */
+@WebServlet("/ItemAddServlet")
+public class ItemAddServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)  {
+
+
+		
+		
+	}
+
+}
