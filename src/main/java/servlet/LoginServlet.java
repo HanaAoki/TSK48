da037d6f365216f57de478e71595d840b719c59c
DESKTOP-1E0P1LB\user43 <hana.aoki@comsize.com>
2024-05-14 11:52:09 +0900

    loginServletにcategoryListとstatusListのセッションの追加

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.dao.CategoryDAO;
+import model.dao.StatusDAO;
 import model.entity.UserBean;
 
 /**
@@ -51,6 +53,17 @@ public class LoginServlet extends HttpServlet {
 				//実行出来たらuserの名前で中身をセッションに登録
 				session.setAttribute("user", Ub);
 			}
+			
+//			categorydaoのインスタンス化
+			CategoryDAO categoryDAO = new CategoryDAO();
+			
+//			Statusdaoのインスタンス化
+			StatusDAO statusDAO = new StatusDAO();
+			
+//			categorybeanとstatusbeanのlistをセッションに入れる
+			session.setAttribute("categoryList", categoryDAO.selectCategory());
+			session.setAttribute("statusList", statusDAO.selectStatus());
+		
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
