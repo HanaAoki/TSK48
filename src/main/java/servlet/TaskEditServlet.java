7272d617be6bf95a2e8d9911fdcb5d921ed2a15e
specialshige <yasuto.shigematsu@comsize.com>
2024-05-15 11:22:08 +0900

    未完成のタスク編集パーツをプッシュ

---
+++ src/main/java/servlet/TaskEditServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TaskEditServlet
+ */
+/**
+ * タスク編集を行うサーブレットです
+ * @author 
+ */
+@WebServlet("/task-edit-servlet")
+public class TaskEditServlet extends HttpServlet {
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
