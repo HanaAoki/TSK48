fd253678b53acacdf807b06c79694dc26c99e394
specialshige <yasuto.shigematsu@comsize.com>
2024-05-20 10:36:51 +0900

    TaskEditDAOの途中までをプッシュ

--- src/main/java/model/dao/TaskEditDAO.java
+++ src/main/java/model/dao/TaskEditDAO.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -9,27 +10,32 @@ import model.entity.TaskBean;
 
 public class TaskEditDAO {
 
-	public int edittask(TaskBean confirm)
+	public int editTask(TaskBean confirm)
 			throws SQLException, ClassNotFoundException {
 
 		int count = 0;
 
 		String sql = "UPDATE t_task "
 				+ "SET item_name = ?, category_code = ?, price = ? "
+				+ "item_name = ?, category_code = ?, price = ? "
 				+ "WHERE item_code = ? ";
 
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql)) {
 
-			String itemname = confirm.getItemName();
-			int categorycode = confirm.getCategorycode();
-			int price = confirm.getPrice();
-			int itemcode = confirm.getItemCode();
+			String taskname = confirm.getTaskName();
+			int categoryid = confirm.getCategoryId();
+			Date limitdate = confirm.getLimitDate();
+			String price = confirm.getStatusId();
+			int taskid = confirm.getTaskId();
 
-			pstmt.setString(1, itemname);
-			pstmt.setInt(2, categorycode);
-			pstmt.setInt(3, price);
-			pstmt.setInt(4, itemcode);
+			pstmt.setString(1, taskname);
+			pstmt.setInt(2, categoryid);
+			pstmt.setString(3, taskname);
+			pstmt.setString(4, taskname);
+			pstmt.setString(5, taskname);
+			pstmt.setInt(6, price);
+			pstmt.setInt(7, taskid);
 
 			count = pstmt.executeUpdate();
 		}
