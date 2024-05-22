097aaa04c26d5880b36d1e27825c2d870455e2b6
DESKTOP-1E0P1LB\user43 <hana.aoki@comsize.com>
2024-05-22 12:00:42 +0900

    CommentBeanの追加

---
+++ src/main/java/model/entity/CommentBean.java
@@ -0,0 +1,74 @@
+package model.entity;
+
+/**
+ * @author 青木春菜
+ */
+public class CommentBean {
+	private int commentId;
+	private int taskId;
+	private String userId;
+	private String comment;
+	
+	public CommentBean() {
+		
+	}
+
+	/**
+	 * @return commentId
+	 */
+	public int getCommentId() {
+		return commentId;
+	}
+
+	/**
+	 * @param commentId セットする commentId
+	 */
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+
+	/**
+	 * @return taskId
+	 */
+	public int getTaskId() {
+		return taskId;
+	}
+
+	/**
+	 * @param taskId セットする taskId
+	 */
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	/**
+	 * @return userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+
+	/**
+	 * @param comment セットする comment
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+
+}
