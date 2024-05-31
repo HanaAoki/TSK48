1eccba27c19dd4184d734dfeb4284488f2ee38c2
DESKTOP-1E0P1LB\user43 <hana.aoki@comsize.com>
2024-05-31 11:58:09 +0900

    SelectLimitDateTestの実装

--- test/model/dao/SelectLimitDateDAOTest.java
+++ test/model/dao/SelectLimitDateDAOTest.java
@@ -2,6 +2,15 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,9 +18,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SelectLimitDateDAOTest {
+	static List<String[]> data = new ArrayList<String[]>();
+	static SelectLimitDateDAO dao = new SelectLimitDateDAO();
+	static List<LocalDate> limitDateList = null;
+	static List<LocalDate> defaultLimitDateList = new ArrayList<LocalDate>();
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		String f = ("aoki-limt_date.csv");
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)))){
+			String line;
+			br.readLine();
+			while((line = br.readLine()) != null) {
+				String[] list = line.split(",");
+				data.add(list);
+			}
+		}
 	}
 
 	@AfterAll
@@ -24,11 +46,21 @@ class SelectLimitDateDAOTest {
 
 	@AfterEach
 	void tearDown() throws Exception {
+		limitDateList = null;
+		defaultLimitDateList.clear();
 	}
 
 	@Test
 	void test() {
-		fail("まだ実装されていません");
+
+		try {
+			limitDateList = dao.selectLimitDate("aoki");
+		}catch(SQLException | ClassNotFoundException e) {
+		}
+		
+		for(int i = 0; i < data.size(); i++) {
+			assertEquals(LocalDate.parse(data.get(i)[0], DateTimeFormatter.ofPattern("yyyy/MM/dd")),limitDateList.get(i));
+		}
 	}
 
 }
