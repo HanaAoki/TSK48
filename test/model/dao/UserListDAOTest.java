022e5ccf3f9e08ce1acc62c19b7e7db03731d7d2
specialshige <yasuto.shigematsu@comsize.com>
2024-05-31 14:14:09 +0900

    UserListDAOTestの追加

--- test/model/dao/UserListDAOTest.java
+++ test/model/dao/UserListDAOTest.java
@@ -2,16 +2,40 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.entity.UserBean;
+
 class UserListDAOTest {
+
+	static List<String[]> data = new ArrayList<String[]>();
+	static UserListDAO dao = new UserListDAO();
+	static List<UserBean> userList = null;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+
+		String f = ("userList.csv");
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)))) {
+			String line;
+			br.readLine();
+			while ((line = br.readLine()) != null) {
+				String[] list = line.split(",");
+				data.add(list);
+			}
+		}
 	}
 
 	@AfterAll
@@ -24,11 +48,22 @@ class UserListDAOTest {
 
 	@AfterEach
 	void tearDown() throws Exception {
+		userList = null;
 	}
 
 	@Test
-	void test() {
-		fail("まだ実装されていません");
-	}
+	void testSelectUser() {
+		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd");
 
+		try {
+			userList = dao.userList();
+		} catch (SQLException | ClassNotFoundException e) {
+		}
+
+		for (int i = 0; i < userList.size(); i++) {
+			assertEquals(data.get(i)[0], "\"" + userList.get(i).getPassword() + "\"");
+			assertEquals(data.get(i)[1], "\"" + userList.get(i).getUserId() + "\"");
+			assertEquals(data.get(i)[2], "\"" + userList.get(i).getUserName() + "\"");
+		}
+	}
 }
