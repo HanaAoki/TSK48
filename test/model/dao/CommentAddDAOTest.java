0a35ab667570b2daf61d15920c86d8f50bf6ea23
ComsizeHA <hisato.arino@comsize.com>
2024-05-31 11:10:02 +0900

    CommentAddDAOTest追加。

--- test/model/dao/CommentAddDAOTest.java
+++ test/model/dao/CommentAddDAOTest.java
@@ -2,14 +2,25 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class CommentAddDAOTest {
+import model.entity.CommentBean;
 
+class CommentAddDAOTest {
+	static CommentAddDAO dao = new CommentAddDAO();
+	static CommentBean cmt = new CommentBean();
+	static int taskID = 1;
+	static String userId = "arino";
+	static String comment = "本日は晴天なり";
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -24,11 +35,32 @@ class CommentAddDAOTest {
 
 	@AfterEach
 	void tearDown() throws Exception {
+		String sql = "DELETE FROM t_comment WHERE task_id = ? AND user_id = ? AND `comment` = ?";
+		
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			pstmt.setInt(1, taskID);
+			pstmt.setString(2, userId);
+			pstmt.setString(3, comment);
+			
+			pstmt.executeUpdate();
+		}
 	}
 
 	@Test
-	void test() {
-		fail("まだ実装されていません");
+	void testCommentAddDAO() {
+		int count = 0;
+		
+		cmt.setTaskId(taskID);
+		cmt.setUserId(userId);
+		cmt.setComment(comment);
+		
+		try {
+			count = dao.addTask(cmt);
+		}catch(SQLException | ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		assertEquals(1,count);
 	}
 
 }
